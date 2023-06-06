907224de9b924b743cb46982b6fb8cf72252aa98
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-06 14:32:16 +0200

    Add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 
 import React from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+align-items: center;
+background: red;
+min-height: 100vh;
+`;
+
+const StyledPara = styled.p`
+max-width: 20em;
+`;
+
 function App() {
 
   return (
+    <MainContainer>
  <h1>Test</h1>
+ <StyledPara>Test Text Lorem ipsum dolor sit amet consectetur, 
+  adipisicing elit. Eos cumque quam facilis suscipit quaerat 
+  numquam facere iste recusandae error doloremque? Earum 
+  error quibusdam mollitia adipisci aut optio esse cumque 
+  odio!</StyledPara>
+ </MainContainer>
   )
 }
 
