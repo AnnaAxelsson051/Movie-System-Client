7bf4df7cf50f2282aba4fc25a8f49c65d06054c8
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-06 13:11:32 +0200

    Add App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+function App() {
+
+  return (
+ <h1>Test</h1>
+  )
+}
+
+export default App
+
+
+
+/*
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <h1>Test</h1>
+  )
+}
+
+export default App
+ */
