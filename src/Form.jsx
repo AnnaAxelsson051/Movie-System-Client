a33a93db6eeb3b544259160d35881c3003638ff6
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-09 12:51:29 +0200

    Add functionality for selecting genre and movie via input fields

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,19 +2,36 @@ import React, {useState} from "react";
 
 function Form(){
 const [movie,setMovie] = useState('');
+const [genre,setGenre] = useState('');
 
-function handleChange (movieName){
+function handleMovieChange (evt){
 //console.log(movieName.target.value);
-setMovie(movieName.target.value);
+setMovie(evt.target.value);
+}
+
+function handleGenreChange (evt){
+    //console.log(movieName.target.value);
+    setGenre(evt.target.value);
+    }
+
+function handleSubmit (evt){
+console.log(evt)
+evt.preventDefault()
 }
     return (
     <>
     <h1>Form</h1>
     ID
-    <form>
-    <input type="text" 
+    <form onSubmit={handleSubmit}>
+    <input 
+    type="text" 
     value={movie}
-    onChange={handleChange}
+    onChange={handleMovieChange}
+    />
+       <input 
+    type="text" 
+    value={genre}
+    onChange={handleGenreChange}
     />
     <button type = "submit">SEND</button>
     </form>
