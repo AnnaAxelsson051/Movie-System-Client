c9390a510dce09802cbca5e81089198f195ab204
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-10 17:52:09 +0200

    Add functionality for listing genre options in a dropdown menu

--- src/UserMovieAdd.jsx
+++ src/UserMovieAdd.jsx
@@ -2,6 +2,65 @@ import React, { useState } from 'react';
 
 const ADD_MOVIE ="";
 
+const genres = [
+  {
+    id: 28,
+    name: 'Action',
+  },
+  {
+    id: 12,
+    name: 'Adventure',
+  },
+  {
+    id: 35,
+    name: 'Comedy',
+  },
+  {
+    id: 80,
+    name: 'Crime',
+  },
+  {
+    id: 99,
+    name: 'Documentary',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+  },
+  {
+    id: 36,
+    name: 'History',
+  },
+  {
+    id: 27,
+    name: 'Horror',
+  },
+  {
+    id: 9648,
+    name: 'Mystery',
+  },
+  {
+    id: 10749,
+    name: 'Romance',
+  },
+  {
+    id: 878,
+    name: 'Science Fiction',
+  },
+  {
+    id: 53,
+    name: 'Thriller',
+  },
+  {
+    id: 37,
+    name: 'Western',
+  },
+];
+
 function AddMovie() {
  
 
@@ -41,6 +100,9 @@ function AddMovie() {
       <label>
         <select value={genre} onChange={handleGenreChange}>
           <option value="">Select genre</option>
+          {genres.map((genre) => (
+              <option value={genre.id}>{genre.name}</option>
+            ))}
         </select>
       </label>
       <button type="submit">Add Movie</button>
