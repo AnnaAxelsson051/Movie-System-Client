36f44d3e796658453f1db4f05ec2f94becee6ec7
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-10 17:45:16 +0200

    Add functionality for adding movie to user

--- src/UserMovieAdd.jsx
+++ src/UserMovieAdd.jsx
@@ -1,11 +1,51 @@
 import React, { useState } from 'react';
 
+const ADD_MOVIE ="";
+
 function AddMovie() {
  
 
-  return (
   
+    function handleSubmit() {
+    }
+  
+   function handleUserChange() {
+
+  }
+
+   function handleMovieChange() {
+  }
+
+  function handleGenreChange() {
+  }
+
+
+  return (
+    <>
     <h1>Add Movie</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          placeholder="User Id"
+          type="text"
+          onChange={handleUserChange}
+        />
+      </label>
+      <label>
+        <input
+          placeholder="Movie Title"
+          type="text"
+          onChange={handleMovieChange}
+        ></input>
+      </label>
+      <label>
+        <select value={genre} onChange={handleGenreChange}>
+          <option value="">Select genre</option>
+        </select>
+      </label>
+      <button type="submit">Add Movie</button>
+    </form>
+  </>
   );
 }
 
