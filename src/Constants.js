faaaba9b2bc99a5b186ae3731a2c75bcb6748f03
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-11 20:59:39 +0200

    Add constants class and modify add genre class

---
+++ src/Constants.js
@@ -0,0 +1,5 @@
+const API_PREFIX = 'https://localhost:7131';
+
+export const GET_USERS = "";
+
+export const GET_GENRES_BY_USERID = "";
--- src/UserGenreAdd.jsx
+++ src/UserGenreAdd.jsx
@@ -1,7 +1,68 @@
 import React, { useState } from 'react';
 
+
 function AddGenre() {
  
+
+  const genres = [
+    {
+      id: 28,
+      name: 'Action',
+    },
+    {
+      id: 12,
+      name: 'Adventure',
+    },
+    {
+      id: 35,
+      name: 'Comedy',
+    },
+    {
+      id: 80,
+      name: 'Crime',
+    },
+    {
+      id: 99,
+      name: 'Documentary',
+    },
+    {
+      id: 18,
+      name: 'Drama',
+    },
+    {
+      id: 10751,
+      name: 'Family',
+    },
+    {
+      id: 36,
+      name: 'History',
+    },
+    {
+      id: 27,
+      name: 'Horror',
+    },
+    {
+      id: 9648,
+      name: 'Mystery',
+    },
+    {
+      id: 10749,
+      name: 'Romance',
+    },
+    {
+      id: 878,
+      name: 'Science Fiction',
+    },
+    {
+      id: 53,
+      name: 'Thriller',
+    },
+    {
+      id: 37,
+      name: 'Western',
+    },
+  ];
+  
 
   return (
   
