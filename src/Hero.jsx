5aa2e2169f0f078d46388ece6e2c82ec3bb862f2
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-11 21:44:42 +0200

    Modify nav

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,11 +2,45 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Header = styled.h1``;
+const Nav = styled.h2``;
 
+const HeaderBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  height: auto;
+  border: 1px solid #a69064;
+  width: 27em;
+  margin-bottom: 1.2em;
+`;
+
+const NavBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  height: auto;
+  width: 27em;
+  margin-bottom: 1.2em;
+`;
 
 function Hero () {
     return(
+        <HeaderBox>
         <h1>Movie Spot</h1>
+
+    <h2>Nav</h2>
+    <NavBox>
+        <h3>Nav1</h3>
+        <h3>Nav1</h3>
+        <h3>Nav1</h3>
+    {/*<ul>
+        <li>Nav1</li>
+        <li>Nav2</li>
+        <li>Nav3</li>
+    </ul>*/}
+    </NavBox>
+        </HeaderBox>
     
     );
 }
