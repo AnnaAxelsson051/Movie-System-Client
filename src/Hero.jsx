89a323c10b02b5ee49e9b79fad44b110df2b4e7b
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-12 18:49:28 +0200

    Modify color and size o intro text container

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -8,15 +8,18 @@ font-size: 55px;
 letter-spacing: 1.1rem;
 padding:0.5rem;
 `;
-const Nav = styled.h2``;
+const Nav = styled.h3`
+
+`;
 
 const IntroBox = styled.p`
 margin:0.5rem;
-width:42rem;
+width:36rem;
+width:38rem;
 font-size: 20px;
 padding-left:0.6rem;
-padding-top:0.1rem;
-padding-bottom:0.1rem;
+padding-top:0.5rem;
+padding-bottom:0.5rem;
 letter-spacing: 0.1rem;
 color:white;
 background-color:  #a69064;
@@ -24,7 +27,7 @@ background: linear-gradient(to bottom, #d3c097, #988050, #75613a);
 background: linear-gradient(to bottom, rgb(236, 175, 255), rgb(214, 96, 250), rgb(190, 8, 246));
 background: linear-gradient(to bottom, #c6f9ea, #6ef7ce,#20c997);
 background: linear-gradient(to bottom, #d3c097, #988050, #75613a);
-background: linear-gradient(to bottom, rgb(238, 194, 251), rgb(222, 128, 251), rgb(190, 8, 246));
+background: linear-gradient(to right, rgb(238, 194, 251), rgb(222, 128, 251), rgb(216, 106, 250), rgb(238, 194, 251));
 border-radius: 5px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.43);
  // background: linear-gradient(to bottom, #fef4e2, #f7e7c7, #d4bf94, #a69064);
@@ -51,9 +54,12 @@ const TitleBox = styled.div`
   border-radius: 5px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.43);
   background: linear-gradient(to bottom, #fef4e2, #f7e7c7, #d4bf94, #a69064);
+  background: linear-gradient(to bottom, #d3c097, #988050, #63522e);
   background: linear-gradient(to bottom, #d3c097, #988050, #75613a);
+  background: linear-gradient(to bottom, #d3c097, #988050, #63522e);
   width: 50rem;
   margin-bottom: 0.5rem;
+  margin-top: 0.5rem;
 padding-left:1.2rem;
 padding-top:1rem;
 padding-bottom:1rem;
@@ -79,22 +85,24 @@ color:#50401f;
 
 const ImgBox = styled.img`
  height:20px;
- //🎥
+ //🎥📣📽️
 `;
 
 function Hero () {
     return(
         <HeaderBox>
             <TitleBox>
-        <Header>🎞 MOVIE SPOT 📣</Header>
+        <Header>🎞 MOVIE SPOT</Header>
         </TitleBox>
     <NavBox>
-        <h3>📽️ Home</h3>
+        <h3>📽️</h3>
+        <h3>Home</h3>
         <h3>About </h3>
-        <h3>Contact 📞</h3>
+        <h3>Contact</h3>
+        <h3>📽️</h3>
     </NavBox>
     <IntroBox>
-    <p>Add your favourite genres and movies, view and add ratings</p>
+    <p>Collect your favourite genres and movies - and add ratings &nbsp;📣</p>
     </IntroBox>
     <img src={mob_films} alt="Logo" height="650px"/>
     </HeaderBox> 
