bc908e7a8f66419f57ac3bb3d80a12222bb48e87
Anna Axelsson <a.axelsson51@gmail.com>
2023-06-14 15:17:37 +0200

    Add constants

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# MOVIE SYSTEM
+# MOVIE SYSTEM CLIENT
 
 ## Description 
 
-This is an application where users can add their favourite genres and movies on a personal users page - as well as add ratings for movies they have watched. Users are rendered on the page from a database set up in Azure Datastudio and information is exchanged with the database via a [Movie System Server](https://github.com/AnnaAxelsson051/Movie-System-Server) I have built. The server is built with C# / .NET and it also fetches data from The Movie Database (TMDB) via an external API.
+This is an application where users can add their favourite genres and movies to a personal users page - as well as add ratings for movies they have watched. Users are rendered on the page from a database set up in Azure Datastudio and information is exchanged with the database via a [Movie System Server](https://github.com/AnnaAxelsson051/Movie-System-Server) I have built. The server is built with C# / .NET and it also fetches data from The Movie Database (TMDB) via an external API.
 
 
 ---
--- src/Constants.js
+++ src/Constants.js
@@ -1,18 +1,34 @@
 const API_PREFIX = 'https://localhost:7048';
 
-export const GET_USERS = "";
+//URLs for the API 
+export const GET_USERS = `${API_PREFIX}/Get/User`;
 
-export const GET_GENRES_BY_USERID = "";
+export const GET_GENRES_BY_USERID = (userId) =>
+  `${API_PREFIX}/Get/UserGenre?Id=${userId}`;
 
-export const GET_MOVIES_BY_USERID = "";
+export const GET_MOVIES_BY_USERID = (userId) =>
+  `${API_PREFIX}/Get/UserMovie?Id=${userId}`;
 
-export const GET_MOVIES_AND_RATING_BY_USERID = "";
+export const GET_MOVIES_AND_RATING_BY_USERID = (userId) =>
+  `${API_PREFIX}/Get/MoviesRating?Id=${userId}`;
 
-export const ADD_MOVIE_BY_USERID_GENREID_AND_MOVIENAME = "";
+export const ADD_GENRE_BY_USERID_AND_GENREID = (userId, genreId) =>
+  `${API_PREFIX}/Post/AddGenre?userId=${userId}&genreId=${genreId}`;
 
-export const ADD_GENRE_BY_USERID_AND_GENREID = "";
+export const ADD_MOVIE_BY_USERID_GENREID_AND_MOVIENAME = (
+  userId,
+  genreId,
+  movieName
+) =>
+  `${API_PREFIX}/Post/AddMovie?userId=${userId}&genreId=${genreId}&movie=${movieName}`;
 
-export const ADD_RATING_BY_USERID_RATING_AND_MOVIENAME = "";
+export const ADD_RATING_BY_USERID_RATING_AND_MOVIENAME = (
+  userId,
+  rating,
+  movieName
+) =>
+  `${API_PREFIX}/Post/AddRating?userId=${userId}&rating=${rating}&movie=${movieName}`;
+
   
 
 
